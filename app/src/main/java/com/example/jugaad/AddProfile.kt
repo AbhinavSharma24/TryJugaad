6bfa2fc1a8c35cdaee082dbc7c9af28a7cdf2215
abhinav <abhinavsharma24999@gmail.com>
2019-08-03 23:55:26 +0530

    Reading Contacts and taking profile info

--- app/src/main/java/com/example/jugaad/AddProfile.kt
+++ app/src/main/java/com/example/jugaad/AddProfile.kt
@@ -3,6 +3,7 @@ package com.example.jugaad
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_add_profile.*
 
 class AddProfile : AppCompatActivity() {
@@ -10,6 +11,9 @@ class AddProfile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_profile)
+
+        dummy.requestFocus()
+        dummy.isFocusableInTouchMode = true
 
         addProfileBtn.setOnClickListener {
             startActivity(Intent(this,AddedList::class.java))
